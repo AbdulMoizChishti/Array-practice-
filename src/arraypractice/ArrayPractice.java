d3f3d0ae3e3a0bd8df90d0165fafe790d0546a62
AbdulMoizChishti <moaiz599@gmail.com>
2021-09-21 04:03:36 +0500

    Array Practice

---
+++ src/arraypractice/ArrayPractice.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arraypractice;
+import java.util.Scanner;
+/**
+ *
+ * @author Abdul Moiz Chishti
+ */
+public class ArrayPractice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+ 
+        Scanner obj =new Scanner(System.in);
+    int a[]=new int [5];
+    
+//    a[0]=1;
+//    a[1]=100;
+    
+//        int i;
+//        for( i=0; i< a.length;i++){
+//            System.out.println("Enter any Int");
+//        a[i]=obj.nextInt();
+//        
+//            }
+//        System.out.println(a[i]);
+        
+//        for(int i=1; i< 5 ;i++){
+//            System.out.println(i);}
+//
+//        
+        
+        
+        
+        
+        int d=0;
+        System.out.println(d++);
+//        System.out.println(++d);
+        System.out.println(d++);
+        System.out.println(d);
+        
+        
+
+//        System.out.println(a[1]);
+    
+    }
+    
+}
